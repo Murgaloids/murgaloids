6fb6a4f6aa55b16be2fa759735ce2ba901a2d6ee
David Van <David@DavidVan.net>
2018-03-18 13:56:40 -0700

    Moved item search to item controller.

--- src/main/java/com/murgaloids/server/item/ItemRepository.java
+++ src/main/java/com/murgaloids/server/item/ItemRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 /**
  * This will be AUTO IMPLEMENTED by Spring into a Bean called itemRepository.
