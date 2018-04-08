804082601bc7e8bd3073de24f53d71dbef6cad5c
David Van <David@DavidVan.net>
2018-04-08 16:12:18 -0700

    Added Repository for ItemPhoto table.

---
+++ src/main/java/com/murgaloids/server/item/ItemPhoto.java
@@ -0,0 +1,40 @@
+package com.murgaloids.server.item;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import java.util.Date;
+
+/**
+ * Item is a class representation of an item.
+ */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "item_photo")
+public class ItemPhoto {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id", columnDefinition = "int(11)")
+    private Long id;
+
+    @NonNull
+    @Column(name = "item_id", columnDefinition = "int(11)")
+    private Long itemId;
+
+    @NonNull
+    @Column(name = "image_path", columnDefinition = "varchar(254)")
+    private Long imagePath;
+}
